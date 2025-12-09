ab15a21d87e21d0225bb11ed64cd4399c4361fa4
Койбаев Темирлан <tkojbaev@gmail.com>
2025-12-09 16:57:26 +0300

    backend: add category filter, pagination and updated templates

--- apple_store/urls.py
+++ apple_store/urls.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from django.urls import path
-from shop import views
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.product_list, name='product_list'),
-    path('product/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('', include('shop.urls')),   # ← подключаем shop.urls
 ]
